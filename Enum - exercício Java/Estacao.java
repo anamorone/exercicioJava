cc69365446c30fd00e0ba3796cc98f5b8446bfc1
Ana Carolina Morone <83526857+anamorone@users.noreply.github.com>
2025-10-09 00:29:59 -0300

    Add files via upload

---
+++ Enum - exercício Java/Estacao.java
@@ -0,0 +1,10 @@
+       public enum Estacao{
+           Verao(30, "Verão"), Inverno(19, "Inverno");
+           
+           private int temp;
+           private String nome;
+           
+           Estacao(int t, String n){ temp = t; nome = n; }
+       }
+        
+    
