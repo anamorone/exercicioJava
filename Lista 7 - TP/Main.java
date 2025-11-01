1a5d95d9c0cf89aa121ba990c9a6478e8190e210
Ana Carolina Morone <83526857+anamorone@users.noreply.github.com>
2025-11-01 01:00:57 -0300

    Add files via upload

--- Lista 7 - TP/Main.java
+++ Lista 7 - TP/Main.java
@@ -1,6 +1,9 @@
 /******************************************************************************
 
-EXERCÍCIO 2
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
 *******************************************************************************/
 
 import java.util.*;
@@ -8,37 +11,36 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		ArrayList listA = new ArrayList();
-		ArrayList listB = new ArrayList();
-		Scanner teclado = new Scanner(System.in);
-		
-		listA.add(teclado.nextInt());
-		listA.add(teclado.nextInt());
-		listA.add(teclado.nextInt());
-		listA.add(teclado.nextInt());
-		
-		listB.add(teclado.nextInt());
-		listB.add(teclado.nextInt());
-		listB.add(teclado.nextInt());
-		listB.add(teclado.nextInt());
-		listB.add(teclado.nextInt());
-		
-		ArrayList listC = new ArrayList();
-		
-		for(int i = 0; i < listA.size(); i++){
-		    listC.add(listA.get(i));
-		
-	    }
-	    for(int i = 0; i < listB.size(); i++){
-	        if (!(listC.contains(listB.get(i)))){
-	            listC.add(listB.get(i));
-	        
-        	   }
-	    }
-	Iterator i = listC.iterator();
-	while(i.hasNext()){
-	    System.out.println(i.next());
-	}
+    
+    Scanner teclado = new Scanner(System.in);
+    ArrayList<Integer> listA = new ArrayList<>();
+    ArrayList<Integer> listB = new ArrayList<>();
+    ArrayList<Integer> listC = new ArrayList<>();
+    
+    listA.add(teclado.nextInt());
+    listA.add(teclado.nextInt());
+    listA.add(teclado.nextInt());
+	listA.add(teclado.nextInt());
+	listA.add(teclado.nextInt());
 	
+	listB.add(teclado.nextInt());
+	listB.add(teclado.nextInt());
+	listB.add(teclado.nextInt());
+	listB.add(teclado.nextInt());
+	listB.add(teclado.nextInt());
+	
+	
+for(int i = 0; i < listB.size(); i++){
+    if(listA.contains(listB.get(i))){
+    
+        listC.add(listB.get(i));
+    }
+    
+}
+
+	Iterator it = listC.iterator();
+	while(it.hasNext()){
+	    System.out.println(it.next());
+	}
 }
 }
