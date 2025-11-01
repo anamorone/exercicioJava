b3f4454a420fd649088ae56d50c0ad6ffcb3b004
Ana Carolina Morone <83526857+anamorone@users.noreply.github.com>
2025-11-01 00:37:01 -0300

    Update Main.java

--- Lista 7 - TP/Main.java
+++ Lista 7 - TP/Main.java
@@ -1,9 +1,6 @@
 /******************************************************************************
 
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
+EXERCÍCIO 2
 *******************************************************************************/
 
 import java.util.*;
